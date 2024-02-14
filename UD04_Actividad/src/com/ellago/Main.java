6583713f6b2693cbb0e22ce3b82016a9b9f62b0c
Sarutxi <158266879+Sarutxi@users.noreply.github.com>
2024-02-14 10:17:32 +0100

    Subida proyecto

---
+++ UD04_Actividad/src/com/ellago/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ellago;
+/**
+ * 
+ * @author Ivan y Sara
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta miCuenta;
+        double saldoActual;
+
+  //Soy Iván
+        //Y yo Sara
+
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            miCuenta.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            miCuenta.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
