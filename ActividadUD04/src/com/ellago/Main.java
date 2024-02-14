7255f06dd68bc9c45105b2ff1210477aa21d10e5
iviguefer <158267059+iviguefer@users.noreply.github.com>
2024-02-14 09:09:56 +0100

    hOLA sOY iVÁN

--- ActividadUD04/src/com/ellago/Main.java
+++ ActividadUD04/src/com/ellago/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         CCuenta miCuenta;
         double saldoActual;
-
+  //Soy Iván
         miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = miCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
